e2c1b3a87087135250e378310a9cd472f054588d
Chris Lucynski <chris.lucynski@gmail.com>
2019-12-16 20:00:54 -0700

    test

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client/cbuild")))
+  console.log('hit server file prod');
+  app.use(express.static(path.join(__dirname, "client/build")))
   app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
